e788cfa4713efc9b60fef02ffc98cf9b351b8292
Drew Mullaney <drewmullaney@gmail.com>
2023-11-20 14:41:37 -0700

    initial

---
+++ index.js
@@ -0,0 +1,23 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import { getData } from "./getData.js";
+import { COUNTIES, DATA } from "./data.js";
+
+
+//const URL = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json"
+//const DATA = await getData(URL);
+
+
+
+//#region global vars  
+
+//#endregion
+
+
+//#region scaling and axis stuff  
+
+//#endregion
+
+
+//#region render stuff  
+
+//#endregion
